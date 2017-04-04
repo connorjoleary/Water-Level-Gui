951bc5b1a97c989632b874822c16282ba07d5b0f
fippy24 <connor.trumpet@gmail.com>
2017-04-03 19:51:27 -0600

    Seperation of functions and now looks better

---
+++ buttons.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+def tankReName(num):
+    create_window()
+
+def getValues():
+        print ("getting Vals")
+
+def create_window():
+    t = tk.Toplevel()
+    l = tk.Label(t, text="Here")
+    l.pack(side="top", fill="both", expand=True, padx=100, pady=100)
--- main.py
+++ main.py
@@ -4,30 +4,36 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 from matplotlib.figure import Figure
 
 import tkinter as tk
-from tkinter import ttk
+#from tkinter import ttk
 
-LARGE_FONT= ("Verdana", 12)
+from buttons import *
 
 import numpy as np
-data = np.genfromtxt('test.csv', delimiter=',', names=['x', 'y'])
-data2 = np.genfromtxt('data1.csv', delimiter=',', names=['date', 'level'])
-f = Figure(figsize=(5,5), dpi=100)
-
-Tanks = []
+# data = np.genfromtxt('test.csv', delimiter=',', names=['x', 'y'])
+# data2 = np.genfromtxt('
+# data1.csv', delimiter=',', names=['date', 'level'])
+figs = [Figure(figsize=(8,3), dpi=100), Figure(figsize=(8,3), dpi=100)]
+values=["Tank 1",.26, .025, "Tank 2", .26, .025]
 
 def update(f):
-    f.clear()
-    a = f.add_subplot(2,2,2)
-    data = np.genfromtxt('test.csv', delimiter=',', names=['x', 'y'])
-    a.plot(data['x'], data['y'], color='r', label='the data')
+    for f in figs:
+        f.clear()
+        a = f.add_subplot(1,2,1)
+        data = np.genfromtxt('data1.csv', delimiter=',', names=['time', 'level'])
+        dayData = data[-5:]
 
-    a2 = f.add_subplot(2,2,4)
-    data2 = np.genfromtxt('data1.csv', delimiter=',', names=['date', 'level'])
-    a2.plot(data2['date'], data2['level'], color='r', label='Tank 2')
-    f.canvas.draw()
+        a.plot(dayData['time'], dayData['level'], color='g', label='One Day')
+        a.set_title('One Day')
+        a.set_ylim(ymin=0)
+
+        a2 = f.add_subplot(1,2,2)
+        a2.plot(data['time'], data['level'], color='g', label='One Week')
+        a2.set_title('One Week')
+        a2.set_ylim(ymin=0)
+        f.canvas.draw()
 
     print ("update")
-    main.after(5000, update, f)
+    main.after(5000, update, figs)
 
 class mainP(tk.Tk):
 
@@ -56,29 +62,32 @@ class mainP(tk.Tk):
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label = tk.Label(self, text="Start Page", font=("Helvetica", 32))
         label.pack(pady=10,padx=10)
 
-        button = ttk.Button(self, text="Graphs",
+        button = tk.Button(self, text="Graphs",
                             command=lambda: controller.show_frame(GraphPage))
         button.pack()
-        button2 = ttk.Button(self, text="Settings",
+        button2 = tk.Button(self, text="Settings",
                             command=lambda: controller.show_frame(SettingsPage))
         button2.pack()
-        button3 = ttk.Button(self, text="Information",
+        button3 = tk.Button(self, text="Information",
                             command=lambda: controller.show_frame(InfoPage))
         button3.pack()
 
 class InfoPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Information", font=LARGE_FONT)
+        label = tk.Label(self, text="Information", font=("Helvetica", 32))
         label.grid(row=0, column=1)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.grid()
 
 
 class SettingsPage(tk.Frame):
     def __init__(self, parent, controller):
-        self.values=["Tank 1", "Tank 2", .26, .025, 7]
         tk.Frame.__init__(self,parent)
         label = tk.Label(self, text="Settings", font=("Helvetica", 32))
         label.grid(row=0, column=0, columnspan=3)
@@ -105,35 +114,39 @@ class SettingsPage(tk.Frame):
         e5 = tk.Entry(self)
         e5.grid(padx=10,row=6, column=2)
 
-        button = ttk.Button(self, text="Enter",
+        button = tk.Button(self, text="Enter",
                             command=lambda: getValues())
-        button.grid()
+        button.grid(column=2)
 
-        def getValues():
-            print ("getting Vals")
-            self.values=[e1,e2,e3,e4,e5]
+        button1 = tk.Button(self, text="Back to Home",  height = 2, width = 14,
+                            command=lambda: controller.show_frame(StartPage))
+        button1.grid(columnspan=3)
+
 
 
 class GraphPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
+        label = tk.Label(self, text="Graph Page", font=("Helvetica", 32))
+        label.grid(columnspan=2,pady=10,padx=10)
+        i=0
+        for f in figs:
 
-        button1 = ttk.Button(self, text="Back to Home",
+            label = tk.Label(self, text=values[0+i*3]+"\n Battery: "+str(values[1+i*3]), font=("Helvetica", 20))
+            label.grid(row=i+1,column=0,padx=10)
+
+            canvas = FigureCanvasTkAgg(f, self)
+            canvas.show()
+            #canvas.get_tk_widget().grid(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+            canvas._tkcanvas.grid(row=i+1,column=1)
+
+            i+=1
+
+        button1 = tk.Button(self, text="Back to Home", height = 2, width = 14, 
                             command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-        
-
-        canvas = FigureCanvasTkAgg(f, self)
-        canvas.show()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
-        toolbar = NavigationToolbar2TkAgg(canvas, self)
-        toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        button1.grid(columnspan=2)
 
 main = mainP()
-main.after(1, update, f)
+main.after(1, update, figs)
 main.mainloop()
