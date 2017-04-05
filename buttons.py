82467b27f4f8ee2f2b1ca8a4e03bf33add46d639
fippy24 <connor.trumpet@gmail.com>
2017-04-04 21:51:44 -0600

    Gui now works to specifications but is not pretty

--- buttons.py
+++ buttons.py
@@ -1,27 +1,36 @@
 import tkinter as tk
 
-def tank_rename(num):
-    t = tk.Toplevel()
-    tk.Label(t, text="Enter Tank "+str(num)+"'s name").pack(side="top", fill="both", expand=True, padx=10, pady=5)
-    e = tk.Entry(t)
-    e.pack(padx=10)
-    button = tk.Button(t, text="Enter",
-                        command=lambda: get_values(t, e.get(), 1, num)).pack()
+# def tank_rename(num):
+#     t = tk.Toplevel()
+#     tk.Label(t, text="Enter Tank "+str(num)+"'s name").pack(side="top", fill="both", expand=True, padx=10, pady=5)
+#     e = tk.Entry(t)
+#     e.pack(padx=10)
+#     button = tk.Button(t, text="Enter",
+#                         command=lambda: get_values(t, e.get(), 1, num)).pack()
 
-def conv_factors(num):
-    t = tk.Toplevel()
-    l = tk.Label(t, text="Here")
-    l.pack(side="top", fill="both", expand=True, padx=100, pady=100)
+# def conv_factors(self, num):
+#     t = tk.Toplevel()
+#     tk.Label(t, text="Enter Tank "+str(num+1)+"'s Water Conversion Factor").pack(side="top", fill="both", expand=True, padx=10, pady=5)
+#     e = tk.Entry(t)
+#     e.pack(padx=10)
+#     button = tk.Button(t, text="Enter",
+#                         command=lambda: self.get_values(t, e.get(), 1, num)).pack()
+#     tk.Label(t, text="Enter Tank "+str(num+1)+"'s Battery Conversion Factor").pack(side="top", fill="both", expand=True, padx=10, pady=5)
+#     e2 = tk.Entry(t)
+#     e2.pack(padx=10)
+#     button = tk.Button(t, text="Enter",
+#                         command=lambda: self.get_values(t, e2.get(), 2, num)).pack()
 
-def get_values(t, string, context, num):
-    with open('values.txt', 'r') as file:
-        # read a list of lines into data
-        data = file.readlines()
+# # def get_values(t, string, context, num):
+#     with open('values.txt', 'r') as file:
+#         # read a list of lines into data
+#         data = file.readlines()
 
-    # change the line
-    data[num*context] = string
+#     # change the line
+#     data[num*context] = string+"\n"
 
-    # and write everything back
-    with open('stats.txt', 'w') as file:
-        file.writelines( data )
-    t.destroy()
+#     # and write everything back
+#     with open('values.txt', 'w') as file:
+#         file.writelines(data)
+    
+#     t.destroy()
--- main.py
+++ main.py
@@ -6,21 +6,29 @@ from matplotlib.figure import Figure
 import tkinter as tk
 #from tkinter import ttk
 
-from buttons import *
+#from buttons import *
 import os
 import numpy as np
 # data = np.genfromtxt('test.csv', delimiter=',', names=['x', 'y'])
 # data2 = np.genfromtxt('
 # data1.csv', delimiter=',', names=['date', 'level'])
 figs = [Figure(figsize=(8,3), dpi=100), Figure(figsize=(8,3), dpi=100)]
-if not os.path.exists('./values.txt'):
-    file = open('values.txt', 'w')
+file = open('values.txt', 'r') 
+data = file.readlines()
+file.close()
+if len(data) <= 0:
+    print ("got here")
     i=0
     for f in figs:
+        print ("got here")
         i+=1
-        file.write("Tank "+str(i)+"\n")
-        file.write(str(.26)+"\n")
-        file.write(str(.025)+"\n")
+        data.append("Tank "+str(i)+"\n")
+        data.append(str(.26)+"\n")
+        data.append(str(.025)+"\n")
+
+    file = open('values.txt', 'w')
+    file.writelines(data)
+    file.close()
 
 def update(f):
     for f in figs:
@@ -38,6 +46,7 @@ def update(f):
         a2.set_title('One Week')
         a2.set_ylim(ymin=0)
         f.canvas.draw()
+
 
     print ("update")
     main.after(5000, update, figs)
@@ -86,11 +95,14 @@ class InfoPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
         label = tk.Label(self, text="Information", font=("Helvetica", 32))
-        label.grid(row=0, column=1)
+        label.pack()
+
+        tk.Label(self, text="Creators", font=("Helvetica", 24)).pack()
+        tk.Label(self, text="Connor O'Leary\nDavid Hamilton\nYue Gong", font=("Helvetica", 12)).pack()
 
         button1 = tk.Button(self, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
-        button1.grid()
+        button1.pack()
 
 
 class SettingsPage(tk.Frame):
@@ -129,8 +141,6 @@ class SettingsPage(tk.Frame):
                             command=lambda: controller.show_frame(StartPage))
         button1.grid(columnspan=3)
 
-
-
 class GraphPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -138,28 +148,86 @@ class GraphPage(tk.Frame):
         label = tk.Label(self, text="Graph Page", font=("Helvetica", 32))
         label.grid(columnspan=2,pady=10,padx=10)
         i=0
-        file = open('values.txt', 'r')
-        data = file.readlines()
+        # file = open('values.txt', 'r')
+        # self.data = file.readlines()
+        print (data[0])
+        self.values = []
+        for dat in data:
+            val =  tk.StringVar()
+            val.set(dat.replace('\n',''))
+            self.values.append(val)
+        print ("Tank 1: "+self.values[0].get())
         for f in figs:
-            tk.Button(self, text=data[i*3].replace('\n',''), height = 1, width = 10, borderwidth=1, font=("Helvetica", 20),
-                            command=lambda i=i: tank_rename(i)).grid(row=i*3+1,column=0,padx=10)
+            content = tk.StringVar()
+            tk.Button(self, text=self.values[i*3].get(), textvariable=self.values[i*3], height = 1, width = 10, borderwidth=1, font=("Helvetica", 20),
+                            command=lambda i=i: self.tank_rename(i)).grid(row=i*5+1,column=0,columnspan=2, padx=10)
             
-            tk.Label(self, text="Battery Level: "+str(45)+"%", font=("Helvetica", 16)).grid(row=i*3+2,column=0,padx=10)
+            lab = tk.Label(self, text="Battery Level: "+str(45)+"%", font=("Helvetica", 16))
+            self.values.append(lab)
+            lab.grid(row=i*5+2,column=0,columnspan=2,padx=10)
 
-            tk.Button(self, text="Conversion Factors\nWater:"+data[i*3+1].replace('\n','')+" Battery:"+data[i*3+2].replace('\n',''), 
-                            height = 2, width = 18, borderwidth=1, font=("Helvetica", 12),
-                            command=lambda i=i: conv_factors(i)).grid(row=i*3+3,column=0,padx=10)
+            tk.Label(self, text="Conversion Factors", font=("Helvetica", 12)).grid(row=i*5+3,column=0,columnspan=2,padx=10)
+            tk.Label(self, text="Water:", font=("Helvetica", 12)).grid(row=i*5+4,column=0,padx=1)
+            tk.Button(self, textvariable=self.values[i*3+1], 
+                            height = 1, width = 4, borderwidth=1, font=("Helvetica", 12),
+                            command=lambda i=i: self.conv_water(i)).grid(row=i*5+4,column=1,padx=10)
+            tk.Label(self, text="Battery:", font=("Helvetica", 12)).grid(row=i*5+5,column=0,padx=1)
+            tk.Button(self, textvariable=self.values[i*3+2], 
+                            height = 1, width = 4, borderwidth=1, font=("Helvetica", 12),
+                            command=lambda i=i: self.conv_battery(i)).grid(row=i*5+5,column=1,padx=10)
+            
 
             canvas = FigureCanvasTkAgg(f, self)
             canvas.show()
             #canvas.get_tk_widget().grid(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-            canvas._tkcanvas.grid(row=i*3+1,column=1, rowspan=3)
+            canvas._tkcanvas.grid(row=i*5+1,column=2, rowspan=5)
 
             i+=1
 
         button1 = tk.Button(self, text="Back to Home", height = 2, width = 14,
                             command=lambda: controller.show_frame(StartPage))
-        button1.grid(columnspan=2)
+        button1.grid(columnspan=3)
+
+    def tank_rename(self, num):
+        t = tk.Toplevel()
+        tk.Label(t, text="Enter Tank "+str(num+1)+"'s name").pack(side="top", fill="both", expand=True, padx=10, pady=5)
+        e = tk.Entry(t)
+        e.pack(padx=10)
+        button = tk.Button(t, text="Enter",
+                            command=lambda: self.get_values(t, e.get(), 0, num)).pack()
+
+    def conv_water(self, num):
+        t = tk.Toplevel()
+        tk.Label(t, text="Enter Tank "+str(num+1)+"'s water conversion").pack(side="top", fill="both", expand=True, padx=10, pady=5)
+        e = tk.Entry(t)
+        e.pack(padx=10)
+        button = tk.Button(t, text="Enter",
+                            command=lambda: self.get_values(t, e.get(), 1, num)).pack()
+
+    def conv_battery(self, num):
+        t = tk.Toplevel()
+        tk.Label(t, text="Enter Tank "+str(num+1)+"'s battery conversion").pack(side="top", fill="both", expand=True, padx=10, pady=5)
+        e = tk.Entry(t)
+        e.pack(padx=10)
+        button = tk.Button(t, text="Enter",
+                            command=lambda: self.get_values(t, e.get(), 2, num)).pack()
+
+    def get_values(self, t, string, context, num):
+        data[3*num+context]=string+"\n"
+        self.values[3*num+context].set(string)
+
+        # with open('values.txt', 'r') as file:
+        #     # read a list of lines into data
+        #     data = file.readlines()
+
+        # change the line 
+
+        # and write everything back
+        # print ("changing: " +string)
+        with open('values.txt', 'w') as file:
+            file.writelines(data)
+        
+        t.destroy()
 
 main = mainP()
 main.after(1, update, figs)
